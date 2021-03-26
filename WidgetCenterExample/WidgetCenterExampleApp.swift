f55d03c8fc994352d6eef1ad50628ca690f82baf
Fernando Cardenas <fernando.cardenas@grandcentrix.net>
2021-03-26 17:21:29 +0100

    Initial Commit

---
+++ WidgetCenterExample/WidgetCenterExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetCenterExampleApp.swift
+//  WidgetCenterExample
+//
+//  Created by Fernando Cardenas on 26.03.21.
+//
+
+import SwiftUI
+
+@main
+struct WidgetCenterExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
