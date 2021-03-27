a349bc93f926143fad9551a4a6785f55ffce625c
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-03-27 15:12:36 +0100

    move widgetCenterSpy to its own file

---
+++ WidgetCenterExampleTests/WidgetCenterSpy.swift
@@ -0,0 +1,31 @@
+//
+//  WidgetCenterSpy.swift
+//  WidgetCenterExampleTests
+//
+//  Created by Fernando Cardenas on 27.03.21.
+//
+
+import WidgetCenterExample
+import WidgetKit
+
+class WidgetCenterSpy: WidgetCenterProtocol {
+    
+    typealias Completion = (Result<[WidgetInfo], Error>) -> Void
+    var configCompletions = [Completion]()
+    
+    func getCurrentConfigurations(_ completion: @escaping Completion) {
+        configCompletions.append(completion)
+    }
+    
+    func complete(withInfos infos: [WidgetInfo], at index: Int = 0) {
+        configCompletions[index](.success(infos))
+    }
+    
+    func complete(withError error: Error, at index: Int = 0) {
+        configCompletions[index](.failure(error))
+    }
+}
+
+struct WidgetInfo: WidgetInfoProtocol {
+    let family: WidgetFamily
+}
--- WidgetCenterExampleTests/WidgetCenterStoreTests.swift
+++ WidgetCenterExampleTests/WidgetCenterStoreTests.swift
@@ -9,28 +9,6 @@ import XCTest
 import WidgetCenterExample
 import WidgetKit
 
-class WidgetCenterSpy: WidgetCenterProtocol {
-    
-    typealias Completion = (Result<[WidgetInfo], Error>) -> Void
-    var configCompletions = [Completion]()
-    
-    func getCurrentConfigurations(_ completion: @escaping Completion) {
-        configCompletions.append(completion)
-    }
-    
-    func complete(withInfos infos: [WidgetInfo], at index: Int = 0) {
-        configCompletions[index](.success(infos))
-    }
-    
-    func complete(withError error: Error, at index: Int = 0) {
-        configCompletions[index](.failure(error))
-    }
-}
-
-struct WidgetInfo: WidgetInfoProtocol {
-    let family: WidgetFamily
-}
-
 class WidgetCenterStoreTests: XCTestCase {
     
     func test_init_doesRetrieveWidgets() {
