d68aee3384a3d07cca647fce15a320a045c233f4
Fernando Cardenas <fernando.cardenasm@gmail.com>
2021-03-27 14:58:12 +0100

    method retrieveInstalledWidgets delivers WidgetSizes when successful

--- WidgetCenterExampleTests/WidgetCenterStoreTests.swift
+++ WidgetCenterExampleTests/WidgetCenterStoreTests.swift
@@ -18,25 +18,25 @@ class WidterCenterStore <MyWidgetCenter: WidgetCenterProtocol>: WidgetStore {
     }
     
     func retrieveInstalledWidgets(completion: @escaping (Result<[WidgetSize], Error>) -> Void) {
-//        widgetCenter.getCurrentConfigurations { [weak self] result in
-//            switch result {
-//            case let .success(widgetInfos):
-//                let widgetFamilies = widgetInfos.compactMap { self?.rawValueString(from: $0) }
-//                completion(.success(widgetFamilies))
-//            case let .failure(error):
-//                completion(.failure(error))
-//            }
-//        }
+        widgetCenter.getCurrentConfigurations { [weak self] result in
+            switch result {
+            case let .success(widgetInfos):
+                let widgetFamilies = widgetInfos.compactMap { self?.rawValueString(from: $0) }
+                completion(.success(widgetFamilies))
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
     }
-
-//    private func rawValueString(from widgetInfo: WidgetInfoProtocol) -> WidgetSize? {
-//        switch widgetInfo.family {
-//        case .systemSmall: return .small
-//        case .systemMedium: return .medium
-//        case .systemLarge: return .large
-//        @unknown default: return nil
-//        }
-//    }
+    
+    private func rawValueString(from widgetInfo: WidgetInfoProtocol) -> WidgetSize? {
+        switch widgetInfo.family {
+        case .systemSmall: return .small
+        case .systemMedium: return .medium
+        case .systemLarge: return .large
+        @unknown default: return nil
+        }
+    }
 }
 
 class WidgetCenterSpy: WidgetCenterProtocol {
@@ -47,10 +47,14 @@ class WidgetCenterSpy: WidgetCenterProtocol {
     func getCurrentConfigurations(_ completion: @escaping Completion) {
         configCompletions.append(completion)
     }
+    
+    func complete(withInfos infos: [WidgetInfo], at index: Int = 0) {
+        configCompletions[index](.success(infos))
+    }
 }
 
 struct WidgetInfo: WidgetInfoProtocol {
-    var family: WidgetFamily
+    let family: WidgetFamily
 }
 
 class WidgetCenterStoreTests: XCTestCase {
@@ -59,6 +63,32 @@ class WidgetCenterStoreTests: XCTestCase {
         let (_, center) = makeSUT()
         
         XCTAssertTrue(center.configCompletions.isEmpty)
+    }
+    
+    func test_retrieveInstalledWidgets_deliversWidgetSizes() {
+        let (sut, center) = makeSUT()
+        
+        let exp = expectation(description: "retrieve completion")
+        sut.retrieveInstalledWidgets { result in
+            switch result {
+            case let .success(receivedSizes):
+                XCTAssertEqual(receivedSizes,
+                               [.small,
+                                .medium,
+                                .large])
+            case .failure:
+                XCTFail("Expected to result successfully, we got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        let infos = [WidgetInfo(family: .systemSmall),
+                     WidgetInfo(family: .systemMedium),
+                     WidgetInfo(family: .systemLarge)]
+        
+        center.complete(withInfos: infos)
+        
+        wait(for: [exp], timeout: 0.1)
     }
     
     private func makeSUT() -> (sut: WidgetStore, widgetCenter: WidgetCenterSpy) {
